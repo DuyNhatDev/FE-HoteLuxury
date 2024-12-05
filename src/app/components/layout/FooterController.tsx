3a157706e95ca393333ec62bbfc764fee2801032
DuyNhatDev <duynhat1507@gmail.com>
2024-12-05 20:00:56 +0700

    update footer

--- src/app/components/layout/FooterController.tsx
+++ src/app/components/layout/FooterController.tsx
@@ -4,7 +4,13 @@ import Footer from "@/app/layout/user/footer";
 
 export default function FooterController() {
   const pathname = usePathname();
-  if (pathname.startsWith("/hotel-manager") || pathname.startsWith("/admin")) {
+  if (
+    pathname.startsWith("/hotel-manager") ||
+    pathname.startsWith("/admin") ||
+    pathname.startsWith("/login") ||
+    pathname.startsWith("/sign-up") ||
+    pathname.startsWith("/forget-password")
+  ) {
     return null;
   }
 
